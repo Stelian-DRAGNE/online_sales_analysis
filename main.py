68b5d9886aab696d275501f0ca4447f6bcbb1fe2
Stelian DRAGNE <stelian.dragne@yahoo.com>
2025-02-05 21:12:25 +0200

    Modificare continut fisier main.py.

--- main.py
+++ main.py
@@ -9,20 +9,13 @@ def main():
     manager = ProductManager()
 
     # Add some arbitrary products to the product manager
-    product1 = Product("Laptop", 1000.00, 5)
-    product2 = Product("Smartphone", 500.00, 10)
-    product3 = Product("Headphones", 150.00, 20)
+    product1 = Product("Bread", 11.00, 2)
+    product2 = Product("Milk", 9.00, 5)
+    product3 = Product("Bananas", 8.00, 3)
 
     manager.add_product(product1)
     manager.add_product(product2)
     manager.add_product(product3)
-
-    # Display all products
-    print("Product Inventory:")
-    manager.display_all_products()
-
-    # Display the total value of the inventory
-    print(manager.total_value())
 
 if __name__ == "__main__":
     main()
